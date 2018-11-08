519761b04ec807f51b755055d214ebce90162ed0
AlbertMillan <millana@tcd.ie>
2018-11-08 21:22:06 +0000

    Added Navbar and About section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
